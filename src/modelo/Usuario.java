6785aadaf6f2ade6bef4044470f857db7058ce61
Juan Nebel <jfnebeldunn@gmail.com>
2020-01-12 15:17:05 -0500

    Clase abstracta usuario

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -9,6 +9,18 @@ package modelo;
  *
  * @author Juan Nebel
  */
-public class Usuario {
+public abstract class Usuario {
+    protected String userName;
+    protected String password;
+    protected String email;
+    protected String cedula;
+
+    public Usuario(String userName, String password, String email, String cedula) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.cedula = cedula;
+    }
+    
     
 }
